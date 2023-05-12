c35d548ed787074f84c5970cfbb353358cf512cd
Pratham <127861601+Pratham71@users.noreply.github.com>
2023-05-12 22:21:49 +0530

    ...

--- cogs/HelpCommand.py
+++ cogs/HelpCommand.py
@@ -21,15 +21,16 @@ class HelpCommand(commands.Cog):
         help_embed.add_field(name=f'**{command_prefix}kick id/@ reason=(if any):**',value='Kicks a user from guild/server.',inline=False)
         help_embed.add_field(name=f'**{command_prefix}ban id/@ reason=(if any):**',value='Bans a user from guild/server.',inline=False)
         help_embed.add_field(name=f'**{command_prefix}unban id/@ reason=(if any):**',value='Unbans a user from guild/sever.',inline=False)
-        help_embed.add_field(name=f'**{command_prefix}timeout id/@ duration(in minutes) reason=(if any)**',value=f'Times Out a user from guild/server.',inline=False)
+        help_embed.add_field(name=f'**{command_prefix}timeout id/@ duration(in minutes) reason=(if any):**',value=f'Times Out a user from guild/server.',inline=False)
+        help_embed.add_field(name=f'**{command_prefix}timedout id/@:**',value=f'Checks if the user is timed or not (returns True if the user is timedout otherwise False).',inline=False)
         help_embed.add_field(name=f'**{command_prefix}UserInfo id/@:**',value='returns the information of a user wiht user ID.',inline=False)
         help_embed.add_field(name=f'**{command_prefix}kill:**',value='Ends the loop cycle of the bot (only for the bot owner).',inline=False)
-        help_embed.add_field(name=f'**{command_prefix}time**',value=f'returns the uptime of {self.client.user}.',inline=False)
-        help_embed.add_field(name='**Auto Logging**',value='*setup a channel named log-channel for auto logging to work!*',inline=False)
+        help_embed.add_field(name=f'**{command_prefix}time:**',value=f'returns the uptime of {self.client.user}.',inline=False)
+        help_embed.add_field(name='**Auto Logging**',value='setup a channel named log-channel for auto logging to work!',inline=False)
         help_embed.add_field(name='**embed:**',value='to be updated.',inline=False)
         help_embed.add_field(name='**Do\'nt Dm the bot**',value='**you will be disapointed!**',inline=False)
-
-        await ctx.reply(embed=help_embed)
+        await ctx.send('Check your direct messages for a list of commands!')
+        await ctx.author.send(embed=help_embed)
 
 async def setup(client):
     await client.add_cog(HelpCommand(client))
