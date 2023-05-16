53ab6850db0ca2ac9273255116d38a71c1d85304
ps4-test <133754458+ps4-test@users.noreply.github.com>
2023-05-16 14:28:51 +0530

    Update Events.py

--- cogs/Events.py
+++ cogs/Events.py
@@ -5,10 +5,7 @@ from discord.ext import commands
 class Events(commands.Cog):
     def __init__(self,client):
         self.client = client
-    
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print('Events.py is ready!')
+   
     
     @commands.Cog.listener()
     async def on_guild_join(self,guild):
@@ -92,5 +89,4 @@ class Events(commands.Cog):
         event_embed.add_field(name='**Role ID:**',value=f'{role.id}',inline=False)
         await log_channel.send(embed=event_embed)
        
-async def setup(client):
-    await client.add_cog(Events(client))
+
